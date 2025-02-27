685bccb22b41f5094d68254da39620767ffdda54
Cesar Ticona <cesarju.cc@gmail.com>
2025-02-27 18:34:28 -0400

    Add data pet information

---
+++ src/data.js
@@ -0,0 +1,99 @@
+const data = [
+  {
+    nombre: "Luna",
+    raza: "Labrador",
+    edad: 3,
+    image: "",
+    enAdopcion: true,
+    fecha: "",
+    ubicacionEncontrado: {
+      calle: "5ta Avenida",
+      numero: 123,
+      ciudad: "Ciudad de Guatemala",
+      provincia: "Guatemala",
+      pais: "Guatemala",
+    },
+    estadoSalud: "Bueno",
+    descripcion:
+      "Luna es un perro muy amigable y juguetón. Fue encontrado abandonado en un parque.",
+    contactoRescate: {
+      nombre: "Refugio Esperanza",
+      telefono: "+502 1234 5678",
+      email: "refugio@esperanza.com",
+    },
+  },
+  {
+    nombre: "Rocky",
+    raza: "Labrador",
+    edad: 5,
+    image: "",
+    enAdopcion: false,
+    fecha: "15-03-2024",
+    lugar: "Refugio Patitas Felices, Monterrey",
+    ubicacionEncontrado: {
+      calle: "Calle Bolívar",
+      numero: 456,
+      ciudad: "La Paz",
+      provincia: "La Paz",
+      pais: "Bolivia",
+    },
+    estadoSalud: "Regular",
+    descripcion:
+      "Rocky es una perrito tranquila y cariñosa. Fue rescatada de la calle con signos de desnutrición.",
+    contactoRescate: {
+      nombre: "Patitas Felices Bolivia",
+      telefono: "+591 7654 3210",
+      email: "patitas@felices.com",
+    },
+  },
+  {
+    nombre: "Bella",
+    raza: "Bulldog",
+    edad: 2,
+    image: "",
+    enAdopcion: true,
+    fecha: "",
+    lugar: "Casa de acogida, Puebla",
+    ubicacionEncontrado: {
+      calle: "Avenida Corrientes",
+      numero: 789,
+      ciudad: "Buenos Aires",
+      provincia: "Buenos Aires",
+      pais: "Argentina",
+    },
+    estadoSalud: "Excelente",
+    descripcion:
+      "Bella es un perrita fuerte y juguetón. Fue encontrado perdido en el centro de la ciudad.",
+    contactoRescate: {
+      nombre: "Refugio Huellitas Argentinas",
+      telefono: "+54 11 9876 5432",
+      email: "huellitas@argentina.com",
+    },
+  },
+  {
+    nombre: "Toby",
+    raza: "Siamesa",
+    edad: 4,
+    image: "",
+    enAdopcion: false,
+    fecha: "10-12-2023",
+    lugar: "Refugio Huellitas de Amor, Querétaro",
+    ubicacionEncontrado: {
+      calle: "Calle San Martín",
+      numero: 321,
+      ciudad: "Santa Cruz de la Sierra",
+      provincia: "Santa Cruz",
+      pais: "Bolivia",
+    },
+    estadoSalud: "Bueno",
+    descripcion:
+      "Toby es una gata tranquila y curiosa. Fue rescatada de un edificio abandonado.",
+    contactoRescate: {
+      nombre: "Gatitos Bolivia",
+      telefono: "+591 6789 1234",
+      email: "gatitos@bolivia.com",
+    },
+  },
+];
+
+export { data };
